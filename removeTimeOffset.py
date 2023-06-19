76cd0a040f19d9ae830a31f93a012a26a16b64dd
unknown <yangli052903@gmail.com>
2023-06-19 11:58:03 -0400

    Remove time offset of test data.

--- removeTimeOffset.py
+++ removeTimeOffset.py
@@ -3,14 +3,14 @@ import csv
 times=[]
 col1=[]
 col2=[]
-with open("VibrationData/Steel/EndMill/12MM/3000RPM/PCB_F18IN_T100_D0p125IN.csv",mode="r") as file:
+with open("VibrationData/ChatterIndicatorTests/PCBData.csv",mode="r") as file:
     csvFile = csv.reader(file)
     for lines in csvFile:
         times.append(float(lines[0]))
         col1.append(float(lines[1]))
         col2.append(float(lines[2]))
 
-filename="VibrationData/Steel/EndMill/12MM/3000RPM/PCB_F18IN_T100_D0p125INFX.csv"
+filename="VibrationData/ChatterIndicatorTests/PCBData.csv"
 with open(filename, 'w',newline="") as csvfile:
     csvwriter = csv.writer(csvfile)
     for reading in range(len(times)):
