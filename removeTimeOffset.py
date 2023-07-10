b5ca7872f60a092349d53025d3875f589eaedbf6
unknown <yangli052903@gmail.com>
2023-07-10 10:04:42 -0400

    Rename and reorder vibrations recently collected.

--- removeTimeOffset.py
+++ removeTimeOffset.py
@@ -1,17 +1,17 @@
 import csv
 
+filename="VibrationData/PerfectoD1/DoF_XRail_YRail.csv"
 times=[]
 col1=[]
 col2=[]
-with open("VibrationData/ChatterIndicatorTests/PCBData.csv",mode="r") as file:
+with open(filename,mode="r") as file:
     csvFile = csv.reader(file)
     for lines in csvFile:
         times.append(float(lines[0]))
         col1.append(float(lines[1]))
         col2.append(float(lines[2]))
 
-filename="VibrationData/ChatterIndicatorTests/PCBData.csv"
-with open(filename, 'w',newline="") as csvfile:
+with open(filename,'w',newline="") as csvfile:
     csvwriter = csv.writer(csvfile)
     for reading in range(len(times)):
         csvwriter.writerow([times[reading]-times[0],col1[reading],col2[reading]])
