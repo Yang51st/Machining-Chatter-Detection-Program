2f0a634c916a7e00eb4f74b770c1f675a511a3d4
Li <liy@hurco.com>
2023-08-08 11:17:35 -0400

    Aligned 5 axis Z cuts.

--- Splitter.py
+++ Splitter.py
@@ -11,12 +11,12 @@ import statistics
 
 offset=0
 
-filenamePCB="VibrationData/HurcoVMX42SRTi/CutsAlongXYZ/PCB_F18IN_T25_D0p125_3000RPM_5AXYZ.csv"
+filenamePCB="VibrationData/HurcoVMX42SRTi/CutsAlongZ/PCB_F18IN_T50_D0p125_3000RPM_5AVZ.csv"
 timesPCB=[]
 col1PCB=[]
 col2PCB=[]
 
-filenameEBI="VibrationData/HurcoVMX42SRTi/CutsAlongXYZ/UnalignedData/EBI_F18IN_T25_D0p125_3000RPM_5AXYZ.csv"
+filenameEBI="VibrationData/HurcoVMX42SRTi/CutsAlongZ/UnalignedData/EBI_F18IN_T50_D0p125_3000RPM_5AVZ.csv"
 timesEBI=[]
 col1EBI=[]
 col2EBI=[]
@@ -72,7 +72,7 @@ class MyFrame(wx.Frame):
                 try:
                     timesPCB.append(float(lines[0]))
                     col1PCB.append(float(lines[1]))
-                    col2PCB.append(float(lines[5]))
+                    col2PCB.append(float(lines[4]))
                 except:
                     pass
             col1PCB=signal.detrend(col1PCB,type="constant")
