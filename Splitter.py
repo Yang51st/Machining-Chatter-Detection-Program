83917c9f6358d1911e9c571b8a3e94c9cc47c0fd
Li <liy@hurco.com>
2023-08-16 16:49:39 -0400

    Align newly collected 5AXYZ data.

--- Splitter.py
+++ Splitter.py
@@ -9,12 +9,12 @@ from scipy.signal import butter
 
 offset=0
 
-filenamePCB="VibrationData/HurcoVMX42SRTi/CutsAlongZ/PCB_F18IN_T50_D0p125_3000RPM_5AVZ.csv"
+filenamePCB="VibrationData/HurcoVMX42SRTi/CutsAlongXYZ/Neue/PCB_T100_0p125IN_6900RPM.csv"
 timesPCB=[]
 col1PCB=[]
 col2PCB=[]
 
-filenameEBI="VibrationData/HurcoVMX42SRTi/CutsAlongZ/UnalignedData/EBI_F18IN_T50_D0p125_3000RPM_5AVZ.csv"
+filenameEBI="VibrationData/HurcoVMX42SRTi/CutsAlongXYZ/Neue/UnalignedData/EBI_T100_0p125IN_6900RPM_5XYZ.csv"
 timesEBI=[]
 col1EBI=[]
 col2EBI=[]
@@ -70,7 +70,7 @@ class MyFrame(wx.Frame):
                 try:
                     timesPCB.append(float(lines[0]))
                     col1PCB.append(float(lines[1]))
-                    col2PCB.append(float(lines[4]))
+                    col2PCB.append(float(lines[1]))
                 except:
                     pass
             col1PCB=signal.detrend(col1PCB,type="constant")
